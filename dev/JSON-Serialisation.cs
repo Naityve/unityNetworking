d11ea8be3793bd19561342de060f066fdaeea1c4
Naityve <longinflorek@gmail.com>
2022-03-28 11:37:01 +0100

    added byte serialisation to JSON-Serialisation.cs

--- dev/JSON-Serialisation.cs
+++ dev/JSON-Serialisation.cs
@@ -15,8 +15,20 @@ public class JsonSerialisationExample : MonoBehaviour
         string json = JsonUtility.ToJson(basicObject);
         Debug.Log(json);
 
+        // JSON
+        
         BasicObject copy = JsonUtility.
         FromJson<BasicObject>(json);
+        Vector3 pos = copy.position;
+        Debug.Log($"{copy.name} await {pos.x}, {pos.y}, {pos.z}");
+
+        // BYTES
+
+        byte[] bytes = Encoding.ASCII.GetBytes(json);
+        Debug.Log($"{bytes[0]:x} {bytes[1]:x} {bytes[2]:x} {bytes[3]:x}");
+
+        string jsonFromBytes = Encoding.ASCII.GetString(bytes);
+        BasicObject copy = JsonUtility.FromJson<BasicObject>(jsonFromBytes);
         Vector3 pos = copy.position;
         Debug.Log($"{copy.name} await {pos.x}, {pos.y}, {pos.z}");
     }
